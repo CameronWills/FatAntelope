3f327759fdc31c26ca0530b3505ddb9b3764645e
Cameron Wills <cameron.w@outlook.com>
2015-08-16 16:26:36 +1000

    Initial commit
    Very early work on a tool to diff / compare .net config files

---
+++ ConfigDiff/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConfigDiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+
+        public static XTree BuildTree(string fileName)
+        {
+            var doc = new XmlDocument();
+            doc.Load(fileName);
+
+            doc.Cho
+
+        }
+
+        public static XNode BuildNode(XmlNode node)
+        {
+            if (node.NodeType == XmlNodeType.Element)
+                return BuildElement(XmlNode);
+
+        }
+
+        public static XNode BuildElement(XmlNode node)
+        {
+            if (node.NodeType == XmlNodeType.Element)
+            {
+
+            }
+        } 
+    }
+}
