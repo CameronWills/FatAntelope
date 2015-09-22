bb170f7294a55bf81cf88c3f42da5b502bbe36d6
Cameron Wills <cameron.w@outlook.com>
2015-09-23 08:29:14 +1000

    Beginnings of filtering matching nodes. Attributes first.

--- ConfigDiff/Xnode.cs
+++ ConfigDiff/Xnode.cs
@@ -8,23 +8,28 @@ using System.Xml;
 
 namespace ConfigDiff
 {
+    /// <summary>
+    /// Type (or strength) of a node match between trees
+    /// </summary>
+    public enum MatchType
+    {
+        Match,
+        Change,
+        NoMatch
+    }
+    
     public class XNode
     {
-        /// <summary>
-        /// Type (or strength) of a node match between trees
-        /// </summary>
-        public enum Matching
-        {
-            NoMatch,
-            Change,
-            Match,
-        }
-
         /// <summary>
         /// The respective System.Xml.XmlNode instance that this XNode maps to from the original XmlDocument.
         /// </summary>
         public XmlNode XmlNode { get; set; }
-        
+
+        /// <summary>
+        /// Path from the root to this node in the tree - does not include order
+        /// </summary>
+        public string Name { get; set; }
+
         /// <summary>
         /// Path from the root to this node in the tree - does not include order
         /// </summary>
@@ -41,19 +46,47 @@ namespace ConfigDiff
         public XNode Parent { get; set; }
 
         /// <summary>
-        /// Collection of child nodes
+        /// Collection of child attribute nodes
         /// </summary>
-        public List<XNode> Children { get; set; }
+        public List<XNode> Attributes { get; private set; }
+
+        /// <summary>
+        /// Collection of child attribute nodes
+        /// </summary>
+        public List<XNode> Elements { get; private set; }
+
+        /// <summary>
+        /// Collection of child text nodes
+        /// </summary>
+        public List<XNode> Texts { get; set; }
 
         /// <summary>
         /// Node in other tree that this node is matched with
         /// </summary>
-        public XNode Match { get; set; }
+        public XNode Matching { get; set; }
 
         /// <summary>
         /// Type (or strength) of match that this node has with it's 'Match' node
         /// </summary>
-        public Matching MatchType { get; set; }
+        public MatchType Match { get; set; }
+
+        /// <summary>
+        /// Collection of child nodes
+        /// </summary>
+        public IEnumerable<XNode> Children 
+        { 
+            get 
+            {
+                foreach (var att in Attributes)
+                    yield return att;
+
+                foreach (var elem in Elements)
+                    yield return elem;
+
+                foreach (var text in Texts)
+                    yield return text;
+            } 
+        }
 
         /// <summary>
         /// Constructor.
@@ -64,7 +97,7 @@ namespace ConfigDiff
         {
             this.XmlNode = node;
             this.Parent = parent;
-            this.Children = new List<XNode>();
+            //this.Children = new List<XNode>();
         }
 
         public bool IsAttribute()
@@ -88,6 +121,7 @@ namespace ConfigDiff
             {
                 var xnode = new XNode(node, parent);
                 var name = "@" + node.Name.ToLowerInvariant();
+                xnode.Name = name;
                 xnode.Path = string.Format("{0}/{1}", path, name);
                 xnode.Hash = Murmur3Hasher.HashString(name + "/" + (node.Value ?? string.Empty));
                 return xnode;
@@ -97,6 +131,7 @@ namespace ConfigDiff
             {
                 var xnode = new XNode(node, parent);
                 var name = "#text";
+                xnode.Name = name;
                 xnode.Path = string.Format("{0}/{1}", path, name);
                 xnode.Hash = Murmur3Hasher.HashString(name + "/" + (node.Value ?? string.Empty));
 
@@ -107,6 +142,7 @@ namespace ConfigDiff
             {
                 var xnode = new XNode(node, parent);
                 var name = node.Name.ToLowerInvariant();
+                xnode.Name = name;
                 xnode.Path = string.Format("{0}/{1}", path, name);
                 var hashes = new List<byte[]>();
                 hashes.Add(Murmur3Hasher.HashString(name + "/"));
@@ -135,7 +171,13 @@ namespace ConfigDiff
             var xChild = XNode.Build(child, parent, parent.Path);
             if (xChild != null)
             {
-                parent.Children.Add(xChild);
+                if (xChild.IsElement())
+                    parent.Elements.Add(xChild);
+                if(xChild.IsAttribute())
+                    parent.Attributes.Add(xChild);
+                if (xChild.IsText())
+                    parent.Texts.Add(xChild);
+
                 hashes.Add(xChild.Hash);
             }
         }
